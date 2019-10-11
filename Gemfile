5f6186be285c0a91d81a5e6309dcf4e7f6cf09f4
Rosy Arreola <rosy.arreola@yahoo.com>
2019-10-11 05:51:45 +0000

    fixed image storage and magick

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 
 # Use ActiveStorage variant
-# gem 'mini_magick', '~> 4.8'
+gem 'mini_magick', '~> 4.8'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
@@ -79,3 +79,4 @@ gem 'carrierwave', '~> 2.0'
 gem 'figaro'
 
 gem 'fog-aws'
+
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,9 @@
 class Course < ApplicationRecord
   belongs_to :user
   has_many :sections
-  mount_uploader :images, ImageUploader
+  mount_uploader :image, ImageUploader
   
   validates :title, presence: true
   validates :description, presence: true
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
-  validates :image, presence: true
 end
