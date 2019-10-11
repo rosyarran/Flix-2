82d8d3ddde3344bc3698f8437e86ac82074c44ae
Rosy Arreola <rosy.arreola@yahoo.com>
2019-10-11 20:32:54 +0000

    sections modal

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :dashboard, only: [:show]
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
@@ -14,7 +15,7 @@ Rails.application.routes.draw do
       resources :lessons, only:[:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create]
+      resources :sections, only: [:create]
     end
   end
 end
