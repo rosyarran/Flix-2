64c6425642451f7929b69f486011a7d75ab901e5
Rosy Arreola <rosy.arreola@yahoo.com>
2019-10-13 01:59:39 +0000

    fixing heroku

--- app/models/section.rb
+++ app/models/section.rb
@@ -7,6 +7,9 @@ class Section < ApplicationRecord
   
   def next_section
     section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
+    if section.blank? && course.next_course
+      return course.next_course.sections.rank(:row_order).first
+    end
     return section
   end
 
